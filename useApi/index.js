d1c63b32e4ca7c98c4d7830da067214781c9eac4
ashu7272 <ashutoshjha7272@gmail.com>
2024-01-07 13:09:26 +0530

    chatting

---
+++ useApi/index.js
@@ -0,0 +1,11 @@
+
+const btnEl = document.getElementById("btn");
+btnEl.addEventListener("click",addPage);
+const apiUrl = " http://127.0.0.1:5000/chatAPI";
+
+
+ async function addPage(){
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+}
